057983617a203002df2e9fccba2a19525f301c0d
Misha Krasnonos <muzishutka7@gmail.com>
2021-06-28 15:59:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ const refs = {
 refs.body.style =
   "display: flex;justify-content: center;margin-top: 80px;font-size: 30px";
 
-const date1 = Date.now();
-
-setTimeout(() => {
-  const date2 = Date.now();
-  console.log("date1", date1);
-  console.log("date2", date2);
-  console.log("Diff", date2 - date1);
-}, 1000);
+const reTimer = {
+  start() {
+    const startTime = Date.now();
+    setInterval(() => {
+      const currentTime = Date.now();
+      console.log("start", startTime);
+      console.log("current", currentTime);
+    }, 1000);
+  },
+};
+reTimer.start();
 
 //Плагин это класс CountdownTimer, экземпляр
 
-new CountdownTimer({
-  selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2021"),
-});
+// const example = new CountdownTimer({
+//   selector: "#timer-1",
+//   targetDate: new Date("Jul 17, 2021"),
+// });
