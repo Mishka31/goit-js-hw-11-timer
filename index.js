9cf0812ddd88baee6406352452f3d8b4dc1d9d74
Misha Krasnonos <muzishutka7@gmail.com>
2021-06-28 23:28:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
 const refs = {
-  body: document.querySelector(".timer"),
-  divEl: document.querySelector(".value"),
+  body: document.querySelector("body"),
+  timerEl: document.querySelector(".timer"),
+  daysEl: document.querySelector('[data-value="days"]'),
+  hoursEl: document.querySelector('[data-value="hours"]'),
+  minsEl: document.querySelector('[data-value="mins"]'),
+  secsEl: document.querySelector('[data-value="secs"]'),
 };
-refs.body.style =
-  "display: flex;justify-content: center;margin-top: 80px;font-size: 30px";
+refs.body.style = "background: coral";
+refs.timerEl.style =
+  "background: red;display: flex;justify-content: center;margin-top: 80px;font-size: 50px";
 
-const reTimer = {
+class CountdownTimer {
+  constructor({ targetDate, onTick }) {
+    this.targetDate = targetDate;
+    this.onTick = onTick;
+  }
   start() {
-    const startTime = Date.now();
     setInterval(() => {
-      const currentTime = Date.now();
-      console.log("start", startTime);
-      console.log("current", currentTime);
+      const timeNow = Date.now();
+      const timeDifferent = this.targetDate - timeNow;
+      const time = getTimeElements(timeDifferent);
+      this.onTick(time);
     }, 1000);
-  },
-};
+  }
+}
+const reTimer = new CountdownTimer({
+  onTick: updateReTime,
+});
+
 reTimer.start();
 
-//Плагин это класс CountdownTimer, экземпляр
+function updateReTime({ days, hours, mins, secs }) {
+  refs.daysEl.textContent = `${days}`;
+  refs.hoursEl.textContent = `${hours}`;
+  refs.minsEl.textContent = `${mins}`;
+  refs.secsEl.textContent = `${secs}`;
+}
 
-// const example = new CountdownTimer({
-//   selector: "#timer-1",
-//   targetDate: new Date("Jul 17, 2021"),
-// });
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+function getTimeElements(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  return { days, hours, mins, secs };
+}
+const example = new CountdownTimer({
+  //   selector: "#timer-1",
+  targetDate: new Date("Jul 17, 2021"),
+});
