95c27734d36d2aed44b477f0177567c761dc273f
Misha Krasnonos <muzishutka7@gmail.com>
2021-06-29 16:47:52 +0300

    update

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const refs = {
   body: document.querySelector("body"),
   timerEl: document.querySelector(".timer"),
-  daysEl: document.querySelector('[data-value="days"]'),
-  hoursEl: document.querySelector('[data-value="hours"]'),
-  minsEl: document.querySelector('[data-value="mins"]'),
-  secsEl: document.querySelector('[data-value="secs"]'),
 };
+
 refs.body.style = "background: coral";
-refs.timerEl.style =
-  "background: red;display: flex;justify-content: center;margin-top: 80px;font-size: 50px";
+refs.timerEl.style = "background: red;display: flex;justify-content: center;margin-top: 80px;font-size: 50px";
+refs.labelEl.style = "padding-right: 15px";
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class CountdownTimer {
   constructor({ targetDate, selector }) {
-    this.bodyEl = document.querySelector(selector);
+    this.selector = selector;
     this.targetDate = targetDate;
+    this.valueDays = document.querySelector(`${selector} [data-value="days"]`);
+    this.valueHours = document.querySelector(`${selector} [data-value="hours"]`);
+    this.valueMins = document.querySelector(`${selector} [data-value="mins"]`);
+    this.valueSecs = document.querySelector(`${selector} [data-value="secs"]`);
   }
   start() {
     setInterval(() => {
@@ -30,24 +31,18 @@ class CountdownTimer {
   }
   getTimeElements(time) {
     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-    const hours = this.pad(
-      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    );
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
     return { days, hours, mins, secs };
   }
-
   updateReTime({ days, hours, mins, secs }) {
-    refs.daysEl.textContent = `${days}`;
-    refs.hoursEl.textContent = `${hours}`;
-    refs.minsEl.textContent = `${mins}`;
-    refs.secsEl.textContent = `${secs}`;
+    this.valueDays.textContent = `${days}`;
+    this.valueHours.textContent = `${hours}`;
+    this.valueMins.textContent = `${mins}`;
+    this.valueSecs.textContent = `${secs}`;
   }
 }
-
-// reTimer.start();
-
 const example = new CountdownTimer({
   selector: "#timer-1",
   targetDate: new Date("Jul 30, 2021"),
