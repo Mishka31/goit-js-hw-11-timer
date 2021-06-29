a928d6b094664e130bba68f80f06105f300ff144
Misha Krasnonos <muzishutka7@gmail.com>
2021-06-29 16:54:13 +0300

    completed

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const refs = {
   body: document.querySelector("body"),
   timerEl: document.querySelector(".timer"),
 };
-
 refs.body.style = "background: coral";
 refs.timerEl.style = "background: red;display: flex;justify-content: center;margin-top: 80px;font-size: 50px";
-refs.labelEl.style = "padding-right: 15px";
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class CountdownTimer {
